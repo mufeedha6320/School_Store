8e283917ded70aa4970711833459fb9566bd3976
Fathimath Mufeedha <106917737+mufeedha6320@users.noreply.github.com>
2023-09-27 20:58:49 +0530

    Update urls.py

--- storeproject/storeapp/urls.py
+++ storeproject/storeapp/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = "storeapp"
 
 urlpatterns = [
-    path('',views.demo,name='demo'),
-    path('login', views.login, name='login'),
+    path('home/',views.demo,name='demo'),
+    #path('login', views.login, name='login'),
     path('register/', views.register, name='register'),
-    path('login/order/',views.order, name='order'),
+    path('home/order/',views.order, name='order'),
     path('logout/', views.logout, name='logout'),
 ]
